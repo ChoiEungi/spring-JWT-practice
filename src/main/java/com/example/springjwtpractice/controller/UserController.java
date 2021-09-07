980e29454cf5fdcf31dadc3dad1cd279fdd7b03d
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-09-07 20:54:09 +0900

    [add] test db insertion

--- src/main/java/com/example/springjwtpractice/controller/UserController.java
+++ src/main/java/com/example/springjwtpractice/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.example.springjwtpractice.controller;
 
 
+import com.example.springjwtpractice.controller.dto.RoleToUserDto;
+import com.example.springjwtpractice.domain.Role;
 import com.example.springjwtpractice.domain.User;
 import com.example.springjwtpractice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,4 +23,25 @@ public class UserController {
     public ResponseEntity<List<User>> getUser(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString()); // return uri on header
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/users")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString()); // return uri on header
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserDto form){
+        userService.addRoleToUser(form.getUsername(), form.getUsername());
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
---
+++ src/main/java/com/example/springjwtpractice/controller/dto/RoleToUserDto.java
@@ -0,0 +1,11 @@
+package com.example.springjwtpractice.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoleToUserDto {
+    private String username;
+    private String roleName;
+}
