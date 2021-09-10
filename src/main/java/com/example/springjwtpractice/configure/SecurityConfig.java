549b7adc964685b8314a1c7cc7fa38c3002ddcb5
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-09-10 11:44:15 +0900

    [add] config

--- src/main/java/com/example/springjwtpractice/configure/SecurityConfig.java
+++ src/main/java/com/example/springjwtpractice/configure/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -25,7 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // make stateless
+        http.authorizeRequests().anyRequest().permitAll(); // allow everyone at first
+        http.addFilter(null); // not filter yet.
     }
 
 }
